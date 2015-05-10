f5f04f368e9a8077ff1676b5838ba3675460a531
Prescott Murphy <prurph@gmail.com>
2015-05-10 16:59:18 -0400

    Add apply to trait and implement for Option and List applicatives.

--- src/main/scala/Applicative.scala
+++ src/main/scala/Applicative.scala
@@ -8,15 +8,31 @@ import simulacrum._
   // rather that, for example, Option models the "effect" of having or not
   // having a value. List models the "effect" of having multiple values.
   def pure[A](a: A): F[A]
+
+  // Takes two proper types, A and B, and an F[A], but instead of taking A => B,
+  // as with Functor's map, takes a type that exists _within_ the type
+  // constructor. Applicative operates inside the "container", Functor "unwraps"
+  // and "rewraps".
+  def apply[A, B](fa: F[A])(ff: F[A => B]): F[B]
+
+  /* Derived methods */
 }
 
 object Applicative {
 
   implicit val optionApplicative: Applicative[Option] = new Applicative[Option] {
     def pure[A](a: A): Option[A] = Some(a)
+
+    def apply[A, B](fa: Option[A])(ff: Option[A => B]): Option[B] = (fa, ff) match {
+      case (None, _) => None
+      case (Some(a), None) => None
+      case (Some(a), Some(f)) => Some(f(a))
+    }
   }
 
   implicit val listApplicative: Applicative[List] = new Applicative[List] {
     def pure[A](a: A): List[A] = List(a)
+    def apply[A, B](fa: List[A])(ff: List[A => B]): List[B] =
+      (fa zip ff).map { case (a, f) => f(a) }
   }
 }
